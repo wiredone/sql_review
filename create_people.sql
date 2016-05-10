2759b27339facdafea2c64166835b81f567ccf98
Matthew Edmondson <mle@wired.one>
2016-05-10 07:54:08 +0100

    usual

--- create_people.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE people (
-       name VARCHAR(255),
-       age INT2,
-       nationality VARCHAR(255),
-       sex CHAR(1)
-);
--- script.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO people (name, age,nationality,sex) VALUES ('Rick',27,'Scottish','M');
-INSERT INTO people (name, age,nationality,sex) VALUES ('Val',33,'Scottish','F');
-INSERT INTO people (name, age,nationality,sex) VALUES ('Val',33,'','');
